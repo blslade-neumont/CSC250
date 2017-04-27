62bcb5a7753ac2ed891d16073ad64968cf114f1c
aboveyou00 <brandonyoyoslade@gmail.com>
2017-04-27 13:41:19 -0600

    feat(lab05): add file io to read the sample files

--- Lab5_Knapsack/src/main.py
+++ Lab5_Knapsack/src/main.py
@@ -1,7 +1,25 @@
 
 
 def main():
-    print('Hello, World!')
+    path = input('Enter a path. (sample0.txt) ') or 'sample0.txt'
+    maxWeight = 0
+    maxItems = 0
+    items = None
+    
+    try:
+        with open(path) as file:
+            lines = [line for line in file if line.strip() != '' and not line.strip().startswith('//')]
+            maxWeight = int(lines[0])
+            maxItems = int(lines[1])
+            items = [tuple(map(str.strip, line.split(','))) for line in lines[2:]]
+    except:
+        print('An Error occurred while reading input file.')
+        return
+    
+    print('maxWeight', maxWeight)
+    print('maxItems', maxItems)
+    for item in items:
+        print(item)
 
 if __name__ == '__main__':
     main()
