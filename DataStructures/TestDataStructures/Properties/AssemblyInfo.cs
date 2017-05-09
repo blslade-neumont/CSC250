f0ef4b39641560e933bdb8b41731922fcaa03a15
aboveyou00 <brandonyoyoslade@gmail.com>
2017-05-09 15:10:14 -0600

    feat(bst): add a unit testing project

---
+++ DataStructures/TestDataStructures/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestDataStructures")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestDataStructures")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f3a592cf-945f-4895-8da5-b9dd68c294a5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DataStructures/TestDataStructures/TestBinarySearchTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgoDataStructures;
+
+namespace TestDataStructures
+{
+    [TestClass]
+    public class TestBinarySearchTree
+    {
+        [TestMethod]
+        public void Test_SimpleAdd()
+        {
+            var bst = new BinarySearchTree<int>();
+
+            bst.Add(50);
+            bst.Add(25);
+            bst.Add(75);
+            bst.Add(15);
+            bst.Add(35);
+
+            Assert.AreEqual("15, 25, 35, 50, 75", bst.InOrder());
+        }
+    }
+}
