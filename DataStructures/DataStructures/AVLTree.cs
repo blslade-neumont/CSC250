5aba05615231cfd32b987550f4676d0f2093c5ef
aboveyou00 <brandonyoyoslade@gmail.com>
2017-05-12 14:22:58 -0600

    feat(avl): add AVLTree and unit tests

---
+++ DataStructures/DataStructures/AVLTree.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDataStructures
+{
+    public class AVLTree<T> : IEnumerable<T>
+        where T : IComparable<T>
+    {
+        public void Add(T value)
+        {
+            if (root == null) root = new Node(value);
+            else
+            {
+                root.Add(value);
+                root = root.Rebalance();
+            }
+            Count++;
+        }
+        public void Remove(T value)
+        {
+            if (root == null) return;
+            bool wasRemoved = false;
+            root = root.Remove(value, ref wasRemoved);
+            if (wasRemoved) Count--;
+        }
+        public void Clear(bool traverse = false)
+        {
+            if (traverse && root != null) root.Clear();
+            root = null;
+            Count = 0;
+        }
+
+        public bool Contains(T value)
+        {
+            return root != null && root.Contains(value);
+        }
+        public int Count { get; private set; } = 0;
+        public int Height()
+        {
+            return root == null ? 0 : root.Height;
+        }
+        public string InOrder()
+        {
+            return enumerableToString(InOrderEnumerable());
+        }
+        public string PreOrder()
+        {
+            return enumerableToString(PreOrderEnumerable());
+        }
+        public string PostOrder()
+        {
+            return enumerableToString(PostOrderEnumerable());
+        }
+        private string enumerableToString(IEnumerable<T> vals)
+        {
+            return string.Join(", ", vals);
+        }
+        public IEnumerable<T> InOrderEnumerable()
+        {
+            if (root != null) return root.ToEnumerable(0);
+            else return Enumerable.Empty<T>();
+        }
+        public IEnumerable<T> PreOrderEnumerable()
+        {
+            if (root != null) return root.ToEnumerable(-1);
+            else return Enumerable.Empty<T>();
+        }
+        public IEnumerable<T> PostOrderEnumerable()
+        {
+            if (root != null) return root.ToEnumerable(1);
+            else return Enumerable.Empty<T>();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return PreOrderEnumerable().GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private Node root;
+
+        private class Node
+        {
+            public Node(T value)
+            {
+                this.value = value;
+            }
+
+            T value;
+            Node left, right;
+
+            public void Add(T value)
+            {
+                if (value.CompareTo(this.value) < 0)
+                {
+                    if (left == null) left = new Node(value);
+                    else
+                    {
+                        left.Add(value);
+                        left = left.Rebalance();
+                    }
+                }
+                else
+                {
+                    if (right == null) right = new Node(value);
+                    else
+                    {
+                        right.Add(value);
+                        right = right.Rebalance();
+                    }
+                }
+            }
+            public Node Remove(T value, ref bool wasRemoved, bool canRemoveSelf = true)
+            {
+                var compare = value.CompareTo(this.value);
+                if (compare == 0 && canRemoveSelf)
+                {
+                    wasRemoved = true;
+                    if (right == null) return left;
+                    Node parent = this, child = right;
+                    while (child.left != null)
+                    {
+                        parent = child;
+                        child = child.left;
+                    }
+                    var replaceValue = child.value;
+                    parent.Remove(child.value, ref wasRemoved, false);
+                    this.value = replaceValue;
+                }
+                else if (compare < 0) left = left.Remove(value, ref wasRemoved);
+                else right = right.Remove(value, ref wasRemoved);
+                return Rebalance();
+            }
+            public Node Rebalance()
+            {
+                var bf = BalanceFactor;
+                if (bf >= 2)
+                {
+                    if (right.BalanceFactor > 0) return rotateLeft();
+                    else return rotateRightLeft();
+                }
+                else if (bf <= -2)
+                {
+                    if (left.BalanceFactor > 0) return rotateLeftRight();
+                    else return rotateRight();
+                }
+                else return this;
+            }
+            private Node rotateLeft()
+            {
+                var oldRight = right;
+                right = oldRight.left;
+                oldRight.left = this;
+                return oldRight;
+            }
+            private Node rotateRight()
+            {
+                var oldLeft = left;
+                left = oldLeft.right;
+                oldLeft.right = this;
+                return oldLeft;
+            }
+            private Node rotateLeftRight()
+            {
+                left = left.rotateLeft();
+                return rotateRight();
+            }
+            private Node rotateRightLeft()
+            {
+                right = right.rotateRight();
+                return rotateLeft();
+            }
+            public void Clear()
+            {
+                if (left != null) left.Clear();
+                if (right != null) right.Clear();
+                left = null;
+                right = null;
+            }
+
+            public bool Contains(T value)
+            {
+                var compare = value.CompareTo(this.value);
+                if (compare == 0) return true;
+                else if (compare < 0) return left != null && left.Contains(value);
+                else return right != null && right.Contains(value);
+            }
+            public IEnumerable<T> ToEnumerable(int order)
+            {
+                if (order < 0) yield return value;
+                if (left != null)
+                {
+                    foreach (T t in left.ToEnumerable(order))
+                        yield return t;
+                }
+                if (order == 0) yield return value;
+                if (right != null)
+                {
+                    foreach (T t in right.ToEnumerable(order))
+                        yield return t;
+                }
+                if (order > 0) yield return value;
+            }
+            public int Height
+            {
+                get
+                {
+                    var leftHeight = (left == null ? 0 : left.Height);
+                    var rightHeight = (right == null ? 0 : right.Height);
+                    return Math.Max(leftHeight, rightHeight) + 1;
+                }
+            }
+            public int BalanceFactor
+            {
+                get
+                {
+                    var leftHeight = (left == null ? 0 : left.Height);
+                    var rightHeight = (right == null ? 0 : right.Height);
+                    return rightHeight - leftHeight;
+                }
+            }
+        }
+    }
+}
---
+++ DataStructures/TestDataStructures/TestAVLTree.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgoDataStructures;
+
+namespace TestDataStructures
+{
+    [TestClass]
+    public class TestAVLTree
+    {
+        [TestMethod]
+        public void Test_Empty()
+        {
+            var avl = new AVLTree<int>();
+
+            Assert.AreEqual(0, avl.Count);
+            Assert.AreEqual(0, avl.Height());
+            Assert.AreEqual("", avl.InOrder());
+            Assert.AreEqual("", avl.PreOrder());
+            Assert.AreEqual("", avl.PostOrder());
+        }
+
+        [TestMethod]
+        public void Test_EnumerablePreOrder()
+        {
+            var avl = new AVLTree<int>();
+
+            avl.Add(25);
+            avl.Add(15);
+            avl.Add(50);
+
+            //  25
+            //15  50
+
+            Assert.AreEqual("25, 15, 50", string.Join(", ", avl.ToArray()));
+        }
+
+        [TestMethod]
+        public void Test_RotateRight()
+        {
+            var avl = new AVLTree<int>();
+
+            avl.Add(50);
+            avl.Add(25);
+            avl.Add(15);
+
+            //  25
+            //15  50
+            
+            Assert.AreEqual("25, 15, 50", avl.PreOrder());
+        }
+
+        [TestMethod]
+        public void Test_RotateLeft()
+        {
+            var avl = new AVLTree<int>();
+
+            avl.Add(15);
+            avl.Add(25);
+            avl.Add(50);
+
+            //  25
+            //15  50
+            
+            Assert.AreEqual("25, 15, 50", avl.PreOrder());
+        }
+
+        [TestMethod]
+        public void Test_RotateLeftRight()
+        {
+            var avl = new AVLTree<int>();
+
+            avl.Add(50);
+            avl.Add(15);
+            avl.Add(25);
+
+            //  25
+            //15  50
+
+            Assert.AreEqual("25, 15, 50", avl.PreOrder());
+        }
+
+        [TestMethod]
+        public void Test_RotateRightLeft()
+        {
+            var avl = new AVLTree<int>();
+
+            avl.Add(15);
+            avl.Add(50);
+            avl.Add(25);
+
+            //  25
+            //15  50
+
+            Assert.AreEqual("25, 15, 50", avl.PreOrder());
+        }
+
+        [TestMethod]
+        public void Test_ComplexBalance()
+        {
+            var avl = new AVLTree<int>();
+
+            avl.Add(5);
+            avl.Add(10);
+            avl.Add(7);
+            avl.Add(2);
+            avl.Add(4);
+            avl.Add(11);
+            avl.Add(12);
+            avl.Add(15);
+            avl.Add(13);
+            avl.Add(14);
+
+            //        7
+            //  4          13
+            //2   5    11      15
+            //       10  12  14
+
+            Assert.AreEqual("7, 4, 2, 5, 13, 11, 10, 12, 15, 14", avl.PreOrder());
+        }
+
+        [TestMethod]
+        public void Test_RemoveBalance()
+        {
+            var avl = new AVLTree<int>();
+
+            avl.Add(5);
+            avl.Add(10);
+            avl.Add(7);
+            avl.Add(2);
+            avl.Add(4);
+            avl.Add(11);
+            avl.Add(12);
+            avl.Add(15);
+            avl.Add(13);
+            avl.Add(14);
+
+            //        7
+            //  4          13
+            //2   5    11      15
+            //       10  12  14
+
+            avl.Remove(13);
+            avl.Remove(14);
+
+            //       7
+            //  4        11
+            //2   5   10    15
+            //            12
+
+            Assert.AreEqual("7, 4, 2, 5, 11, 10, 15, 12", avl.PreOrder());
+        }
+
+        [TestMethod]
+        public void Test_SimpleAdd()
+        {
+            var avl = new AVLTree<int>();
+
+            avl.Add(50);
+            avl.Add(25);
+            avl.Add(75);
+            avl.Add(15);
+            avl.Add(35);
+
+            //     50
+            //  25    75
+            //15  35
+
+            Assert.AreEqual("15, 25, 35, 50, 75", avl.InOrder());
+            Assert.AreEqual("50, 25, 15, 35, 75", avl.PreOrder());
+            Assert.AreEqual("15, 35, 25, 75, 50", avl.PostOrder());
+        }
+
+        [TestMethod]
+        public void Test_SimpleRemove()
+        {
+            var avl = new AVLTree<int>();
+
+            avl.Add(50);
+            avl.Add(25);
+            avl.Add(75);
+            avl.Add(15);
+            avl.Add(35);
+            avl.Remove(50);
+
+            //     75
+            //  25
+            //15  35
+
+            Assert.AreEqual("15, 25, 35, 75", avl.InOrder());
+            Assert.AreEqual("75, 25, 15, 35", avl.PreOrder());
+            Assert.AreEqual("15, 35, 25, 75", avl.PostOrder());
+        }
+
+        [TestMethod]
+        public void Test_Count()
+        {
+            var avl = new AVLTree<int>();
+            Assert.AreEqual(0, avl.Count);
+
+            avl.Add(50);
+            avl.Add(25);
+            avl.Add(75);
+            Assert.AreEqual(3, avl.Count);
+
+            avl.Add(75);
+            Assert.AreEqual(4, avl.Count);
+
+            avl.Add(15);
+            avl.Add(35);
+            Assert.AreEqual(6, avl.Count);
+
+            avl.Remove(50);
+            Assert.AreEqual(5, avl.Count);
+
+            //     75
+            //  25    75
+            //15  35
+
+            Assert.AreEqual("15, 25, 35, 75, 75", avl.InOrder());
+            Assert.AreEqual("75, 25, 15, 35, 75", avl.PreOrder());
+            Assert.AreEqual("15, 35, 25, 75, 75", avl.PostOrder());
+        }
+
+        [TestMethod]
+        public void Test_Height()
+        {
+            var avl = new AVLTree<int>();
+            Assert.AreEqual(0, avl.Height());
+
+            avl.Add(50);
+            Assert.AreEqual(1, avl.Height());
+
+            avl.Add(25);
+            Assert.AreEqual(2, avl.Height());
+
+            avl.Add(75);
+            Assert.AreEqual(2, avl.Height());
+
+            avl.Add(75);
+            Assert.AreEqual(3, avl.Height());
+
+            avl.Add(15);
+            Assert.AreEqual(3, avl.Height());
+
+            avl.Remove(25);
+            Assert.AreEqual(3, avl.Height());
+
+            //     50
+            //  15    75
+            //          75
+        }
+
+        [TestMethod]
+        public void Test_Clear()
+        {
+            var avl = new AVLTree<int>();
+
+            avl.Add(50);
+            avl.Add(25);
+            avl.Add(75);
+            avl.Add(15);
+            avl.Add(35);
+            Assert.AreEqual(5, avl.Count);
+            Assert.AreEqual(3, avl.Height());
+
+            avl.Clear();
+            Assert.AreEqual(0, avl.Count);
+            Assert.AreEqual(0, avl.Height());
+
+            Assert.AreEqual("", avl.InOrder());
+            Assert.AreEqual("", avl.PreOrder());
+            Assert.AreEqual("", avl.PostOrder());
+        }
+
+        [TestMethod]
+        public void Test_Contains()
+        {
+            var avl = new AVLTree<int>();
+
+            Assert.IsFalse(avl.Contains(50));
+
+            avl.Add(50);
+            Assert.IsTrue(avl.Contains(50));
+
+            avl.Add(50);
+            Assert.IsTrue(avl.Contains(50));
+
+            avl.Remove(50);
+            Assert.IsTrue(avl.Contains(50));
+
+            avl.Remove(50);
+            Assert.IsFalse(avl.Contains(50));
+
+            Assert.AreEqual("", avl.InOrder());
+            Assert.AreEqual("", avl.PreOrder());
+            Assert.AreEqual("", avl.PostOrder());
+        }
+
+        [TestMethod]
+        public void ContainsMaxMin()
+        {
+            var avl = new AVLTree<int>();
+
+            avl.Add(24);
+            avl.Add(10);
+            avl.Add(1337);
+            avl.Add(8);
+            avl.Add(12);
+            avl.Add(100);
+            avl.Add(1400);
+            avl.Add(7);
+            avl.Add(9);
+            avl.Add(11);
+            avl.Add(13);
+            avl.Add(90);
+            avl.Add(110);
+            avl.Add(1350);
+            avl.Add(1500);
+
+            //           24
+            //     10          1337
+            //   8   12    100      1400
+            //  7 9 11 13 90 110 1350  1500
+
+            Assert.IsTrue(avl.Contains(7));
+            Assert.IsTrue(avl.Contains(1500));
+        }
+    }
+}
